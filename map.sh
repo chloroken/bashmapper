d68825e8161551865215653454e9b7a340c74542
chloroken <72576960+chloroken@users.noreply.github.com>
2025-10-17 04:33:44 +0000

    Enhance multiflagging and undo capabilities
    Refactor map.sh to support multiflagging and improve undo functionality.

--- map.sh
+++ map.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# TODO
-# //- Append a "new sigs" to report
-# //- Add multiple undo steps
-# - Add multiflagging
-# - Change map <sig> <jcode> to:
-# - - map fetch <jcode> (current system)
-#
-# Convert entire script to python:
-# - use a mock database
-# - store sigs forever
-# - include timestamps
-# - add routing tool?
-
 # [COMMAND]					[BEHAVIOR]
 # map add					additively paste sigs
 # map lazy					'lazy-delete' paste
@@ -26,7 +13,7 @@
 #
 # [SIGNATURES]				[BEHAVIOR]
 # map <sig> <label>			rename a sig (accepts multiple words)
-# map <sig> flag			append "!" to ID (e.g., "ABC!")
+# map flag <sig> <sig>..	append "!" to one or more signatures
 # map <sig> <jcode>			append class/statics/weather to label
 # map del <sig> <sig>..		remove one or more signatures
 
@@ -46,7 +33,7 @@ if [ ! -d "$dir/top/" ]; then
     mkdir "$dir/top/"
 fi
 
-# Undo functionality
+# Undo functionality (3 steps)
 if [[ "$1" == "undo" ]]; then
 	if [ ! -z "$(ls -A $backup1)" ]; then
 		rm -rf "$top"
@@ -76,7 +63,7 @@ elif [[ "$1" == "up" && "${PWD##*/}" != "top" ]]; then
 	cd ".."
 
 # Multi-sig commands ("map <cmd> <sig> <sig>..")
-elif [[ "$1" == "nav" || "$1" == "del" ]]; then
+elif [[ "$1" == "nav" || "$1" == "del" || "$1" == "flag" ]]; then
 	for param in "$@"; do
 
 		# Ensure we're looking at sig IDs (e.g., "ABC")
@@ -91,6 +78,20 @@ elif [[ "$1" == "nav" || "$1" == "del" ]]; then
 			# Delete signatures ("map del <sig> <sig>..")
 			elif [[ "$1" == "del" ]]; then
 				rm -rf "$filename"
+
+			# Flag a signature with "!" (map flag <sig>)
+			elif [[ "$1" == "flag" ]]; then
+			echo "multiflagging"
+				preString="$filename"
+				for (( i=0; i<${#preString}; i++ )); do
+					if [[ "${preString:$i:1}" == " " ]]; then
+						if [[ $i -ge 1 ]]; then
+							break
+						fi
+					fi
+				done
+				postString="${preString:0:$i}!${preString:$i}"
+				mv "$filename" "$postString"
 			fi
 		fi
 	done
@@ -115,12 +116,12 @@ elif [[ "$1" == "add" || "$1" == "lazy" ]]; then
 
 		# Concatenate new string and remove irrelevant bits
 		tailReal=$(echo "$tail" | cut -c1-"$i")
-		newText=$(echo "${head} ${tailReal}" | sed -e 's/Cosmic Signature //' -e 's/Unstable Wormhole//' -e 's/Wormhole//' -e 's/Gas Site //')
+		newText=$(echo "${head} ${tailReal}" | sed -e 's/Cosmic Signature //' -e 's/Unstable Wormhole //' -e 's/Wormhole/—Wormhole—/' -e 's/Gas Site/—Gas Site—/' -e 's/Data Site/—Data Site—/' -e 's/Relic Site/—Relic Site—/')
 
 		# Remove more bits from data/relic sites, but only when they're revealed
 		# This allows for half-scanned stuff to show "Data Site" still, etc
-		if [[ "$newText" == *"Unsecured"* || "$newText" == *"Forgotten"* || "$newText" == *"Ruined"* || "$newText" == *"Central"* ]] ; then
-			newText=$(echo "${newText}" | sed -e 's/Data Site //' -e 's/Relic Site //')
+		if [[ "$newText" == *"Unsecured"* || "$newText" == *"Forgotten"* || "$newText" == *"Ruined"* || "$newText" == *"Central"* || "$newText" == *"Crimson"* || "$newText" == *"Tetrimon"* ]] ; then
+			newText=$(echo "${newText}" | sed -e 's/—Data Site— //' -e 's/—Relic Site— //')
 		fi
 		
 		# Signature 'overwriting' (i.e., which to keep) functionality is
@@ -158,49 +159,34 @@ elif [[ "$1" == "add" || "$1" == "lazy" ]]; then
 elif [[ "${#1}" -eq 3 ]]; then
 	filename=$(find . -maxdepth 1 -iname "${1}*")
 
-	# Flag a signature with "!" (map flag <sig>)
-	if [[ "$2" == "flag" ]]; then
-		preString="$filename"
-		for (( i=0; i<${#preString}; i++ )); do
-			if [[ "${preString:$i:1}" == " " ]]; then
-				if [[ $i -ge 1 ]]; then
-					break
-				fi
-			fi
-		done
-		postString="${preString:0:$i}!${preString:$i}"
-		mv "$filename" "$postString"
-
 	# Naming commands
-	else
-		id=$(echo "$filename" | cut -c1-5)
-		tempname=$(echo "$id" "$2")
-		re='^[0-9]+$'
+	id=$(echo "$filename" | cut -c1-5)
+	tempname=$(echo "$id" "$2")
+	re='^[0-9]+$'
+	
+	# Auto-label signatures ("map <sig> <jcode>")
+	if [[ "${#2}" -eq 6 && "$2" =~ $re ]]; then # jcodes are 6-digit integers
 		
-		# Auto-label signatures ("map <sig> <jcode>")
-		if [[ "${#2}" -eq 6 && "$2" =~ $re ]]; then # jcodes are 6-digit integers
+		# Append class, static, and weather strings
+		newname=$(grep -hr "$2" "$dir/data.txt")
+		mv "$filename" "$filename $newname"
+		cd "$filename $newname"
 			
-			# Append class, static, and weather strings
-			newname=$(grep -hr "$2" "$dir/data.txt")
-			mv "$filename" "$filename $newname"
-			cd "$filename $newname"
-				
-		# Simple relabel ("map <sig> <label>")
-		elif [[ $# -eq 2 ]]; then
-			mv "$filename" "$tempname"
+	# Simple relabel ("map <sig> <label>")
+	elif [[ $# -eq 2 ]]; then
+		mv "$filename" "$tempname"
 
-		# Complex relabel ("map <sig> <label> <label>..")
-		elif [[ $# -gt 2 ]]; then
-			paramStrings=""
-			i=1
-			for param in "$@"; do
-				if ((i>2)); then
-					paramStrings="${paramStrings}$param "
-				fi
-				((i++))
-			done
-			mv "$filename" "$tempname $paramStrings"
-		fi
+	# Complex relabel ("map <sig> <label> <label>..")
+	elif [[ $# -gt 2 ]]; then
+		paramStrings=""
+		i=1
+		for param in "$@"; do
+			if ((i>2)); then
+				paramStrings="${paramStrings}$param "
+			fi
+			((i++))
+		done
+		mv "$filename" "$tempname $paramStrings"
 	fi
 fi
 
